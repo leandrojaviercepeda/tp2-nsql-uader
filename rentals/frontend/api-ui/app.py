8116ea951498f0797750e7914db34c2cb6dc6d0b
Leandro <leandrojaviercepeda@gmail.com>
2020-03-31 01:29:26 -0300

    feat(frontend-rentals): adding api ui for rentals

--- rentals/frontend/api-ui/app.py
+++ rentals/frontend/api-ui/app.py
@@ -1,19 +1,61 @@
-from flask import Flask, render_template, jsonify, redirect
+from flask import Flask, render_template, jsonify, redirect, request, url_for
+import requests
 
 app = Flask(__name__)
 
+api_url_base = 'http://localhost:5000'
 
-@app.route('/')
+actualize_chatpers_list = False
+
+@app.route('/', methods=['GET','POST'])
 def index():
-    message = 'Funciona'
-    """Retorna la pagina index."""
-    return render_template('/index.html', message=message)
+    chapter_status = 'disponible'
 
-@app.route('/pay')
-def about():
+    if request.method == 'POST':
+        if request.form['options'] or actualize_chatpers_list:
+            chapter_status = request.form['options']
+            r = requests.get('{0}/chapter/{1}'.format(api_url_base, chapter_status))
+            chapters = r.json() if r.status_code == 200 else []
+            return render_template('/index.html', chapters=chapters, chapter_status=chapter_status)
+    
+    r = requests.get('{0}/chapter/{1}'.format(api_url_base, chapter_status))
+    available_chapters = r.json() if r.status_code == 200 else []
+
+    return render_template('/index.html', chapters=available_chapters, chapter_status=chapter_status)
+
+
+@app.route('/chapter/<chapter>/rent', methods=['GET'])
+def rent_chapter(chapter):
+    if request.method == 'GET':
+        r = requests.get('{0}/chapter/{1}/rent'.format(api_url_base, chapter))
+        chapters = actualize_chatpers_list = True if r.status_code == 200 else ''
+        
+    actualize_chatpers_list = False
     """Retorna la pagina para pagar."""
-    return render_template('pay.html')
+    return redirect('/')
+
+@app.route('/pay', methods=['GET', 'POST'])
+def pay_chapter():
+    if request.method == 'POST':
+        if request.form['pay'] and request.form['price']:
+            chapter = request.form['pay']
+            price = request.form['price']
+            r = requests.get('{0}/chapter/{1}/pay/{2}'.format(api_url_base, chapter.strip(), price.strip()))
+            chapters = actualize_chatpers_list = True if r.status_code == 200 else ''
+        
+    actualize_chatpers_list = False
+    """Retorna la pagina para pagar."""
+    return redirect('/')
+
+# app name 
+@app.errorhandler(404)
+  
+# inbuilt function which takes error as parameter 
+def not_found(e): 
+# defining function 
+  return render_template("/components/NotFound/index.html") 
+
 
 if __name__ == "__main__":
-    app.run(host='localhost', port='5000', debug=False)
+    app.run(host='localhost', port='3000', debug=True)
 
