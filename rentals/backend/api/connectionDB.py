1eca462dd5d5681066eb0e8f97300c8c0715de8e
Leandro <leandrojaviercepeda@gmail.com>
2020-04-02 19:21:36 -0300

    refactor(backend-rentals): refactor dockerfiles, now works

--- rentals/backend/api/connectionDB.py
+++ rentals/backend/api/connectionDB.py
@@ -2,7 +2,7 @@ import redis
 
 def connect_db():
     """Crear conexion a la base de datos."""
-    conexion = redis.StrictRedis(host='localhost',port=6379,db=0, decode_responses=True)
+    conexion = redis.StrictRedis(host='redis',port=6379,db=0, decode_responses=True)
     if(conexion.ping()):
         print("Conectado al servidor de redis")
     else:
